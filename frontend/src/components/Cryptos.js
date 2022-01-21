59538dc5c5420933125d4fc60eb7776e8fc8df9e
github4maninder <maninder.email@gmail.com>
2022-01-21 11:38:10 -0500

    fix currency in cad

--- frontend/src/components/Cryptos.js
+++ frontend/src/components/Cryptos.js
@@ -277,9 +277,10 @@ const Cryptocurrencies = () => {
           style={{ width: 120 }}
           onChange={(value, text) => handleChange(value, text)}
         >
+          <Option value="1.25">CAD</Option>
           <Option value="1">USD</Option>
           <Option value="0.89">EURO</Option>
-          <Option value="1015">RWF</Option>
+          
         </Select>
       </div>
       <Table
